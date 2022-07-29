6dc23a0be427e22cf7cb9d3c5113e32ad8982d44
Abrosss <nataroro5@gmail.com>
2022-07-29 22:50:37 +0600

    new

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ const app = express()
 
     app.use('/users', users)
 
-            let PORT = process.env.PORT || 3000
+            const PORT = process.env.PORT || 3000
 
 
             app.listen(PORT, () =>{
